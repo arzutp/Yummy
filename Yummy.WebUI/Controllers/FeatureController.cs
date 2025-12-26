a79ca65cc4323d0def5f1e4dd584e0a338f1ecd2
arzutepe <38106558+arzutp@users.noreply.github.com>
2025-12-26 15:54:37 +0300

    added feature controller for admin

---
+++ Yummy.WebUI/Controllers/FeatureController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Yummy.WebUI.Dtos;
+
+namespace Yummy.WebUI.Controllers;
+public class FeatureController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public FeatureController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IActionResult> FeatureList()
+    {
+        var client = _httpClientFactory.CreateClient();
+
+        var response = await client.GetAsync("https://localhost:7114/api/Features");
+
+        if (response == null)
+        {
+            return View();
+        }
+
+        var jsonData = await response.Content.ReadAsStringAsync();
+        var result = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+
+        return View(result);    
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateFeatureDto createFeatureDto)
+    {
+        var client = _httpClientFactory.CreateClient();
+
+        var jsonData = JsonConvert.SerializeObject(createFeatureDto);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+        var responseMessage = await client.PostAsync("https://localhost:7114/api/Features", stringContent);
+
+        if (responseMessage == null)
+        {
+            return View();
+        }
+        
+        return RedirectToAction("FeatureList");
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+
+        await client.DeleteAsync($"https://localhost:7114/api/Features/{id}");
+
+        return RedirectToAction("FeatureList");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Update(int id) 
+    {
+        var client = _httpClientFactory.CreateClient();
+
+        var responseMessage = await client.GetAsync($"https://localhost:7114/api/Features/{id}");
+
+        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+        var value = JsonConvert.DeserializeObject<GetByIdFeatureDto>(jsonData);
+
+        return View(value);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Update(UpdateFeatureDto updateFeatureDto)
+    {
+        var client = _httpClientFactory.CreateClient();
+
+        var jsonData = JsonConvert.SerializeObject(updateFeatureDto);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+        var responeMessage = await client.PutAsync("https://localhost:7114/api/Features", stringContent);
+
+        return RedirectToAction("FeatureList");
+    }
+}
